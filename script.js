dad9b0b6f77b968cec63f745d7da9c86368b41c9
大塚捺子 <stanatsuko725@gmail.com>
2017-11-25 20:14:45 +0900

    今日はスライドを作った

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#slider').bxSlider({
+//    ホリズンタルに置き換える
+    mode: 'horizotal',
+    speed: 1000,
+    pause: 2000,
+    auto: true,
+    pager: false,
+    controls: false,
+    touchEnabled: false
+  });
+});
